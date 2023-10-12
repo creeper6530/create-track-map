f93dfdbaae877543a06688d301594afb866bdde1
creeper6530 <77244144+creeper6530@users.noreply.github.com>
2023-10-12 15:43:12 +0200

    Revert "bumped to 1.20.1 !!nothing else done but bump number to work, might be unstable!! (#3)"
    This reverts commit 0ad7988d5d3155f742ea9833474d0a41ffa4b7d7.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
   maven("https://mvn.devos.one/snapshots/")  // Create Fabric
   maven("https://api.modrinth.com/maven")  // LazyDFU
   maven("https://maven.tterrag.com/")  // Flywheel
-  maven("https://cursemaven.com")
+  maven("https://www.cursemaven.com")  // Forge Config API Port
 }
 
 val shadowDep: Configuration by configurations.creating
